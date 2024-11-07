a36740b0f73ef8fc900faafc6d66671c4df08f07
Joe Mulholland <C21404162@mytudublin.ie>
2024-11-07 22:49:11 +0000

    Jump fix

--- climbing-game/Player.gd
+++ climbing-game/Player.gd
@@ -36,7 +36,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Handle sprint
