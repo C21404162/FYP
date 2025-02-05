a3773bf61e7728e9b951520ebabdd4bd9773d140
Joe Mulholland <C21404162@mytudublin.ie>
2025-02-05 16:18:59 +0000

    Fade-in added

---
+++ climbing-game/world.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+@onready var color_rect = $CanvasLayer/black_screen
+@onready var fade_in = $CanvasLayer/fade_in  # Ensure this is the correct path to your AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	fade_in.play("fade_in")  # Play the "fade_in" animation
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
