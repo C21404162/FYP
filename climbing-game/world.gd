c8b9d6044d85ebed3c503b6afe4e99fdfbccec20
Joe Mulholland <C21404162@mytudublin.ie>
2025-02-13 17:50:23 +0000

    Climbable surfaces added

--- climbing-game/world.gd
+++ climbing-game/world.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var color_rect = $CanvasLayer/black_screen
 @onready var fade_in = $CanvasLayer/fade_in  # Ensure this is the correct path to your AnimationPlayer
+@onready var left_hand = $Player/lefthand
+@onready var right_hand = $Player/righthand
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
