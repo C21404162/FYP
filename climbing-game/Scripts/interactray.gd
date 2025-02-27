2951bc52eec2fb955788884aa59bd1759ddb9b91
Joe Mulholland <C21404162@mytudublin.ie>
2025-02-27 20:15:14 +0000

    raycast testing

--- climbing-game/Scripts/interactray.gd
+++ climbing-game/Scripts/interactray.gd
@@ -1,13 +1,20 @@
 extends RayCast3D
 
+@export var interaction_icon: TextureRect  # Reference to the UI icon
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	#interaction_icon.modulate = Color(1, 1, 1, 0)  # Hide the icon initially
+	create_tween().tween_property(interaction_icon, "modulate", Color(1, 1, 1, 0), 0.1)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	
-	if is_colliding() and is_in_group("Interactable"):
-		print("LOOK")
+	if is_colliding():
+		var collider = get_collider()
+		if collider and collider.is_in_group("Interactable"):
+			# Show the interaction icon
+			create_tween().tween_property(interaction_icon, "modulate", Color(1, 1, 1, 0), 0.1)
+		else:
+			# Hide the interaction icon
+			create_tween().tween_property(interaction_icon, "modulate", Color(1, 1, 1, 0), 0.1)
+	else:
+		# Hide the interaction icon if the raycast isn't colliding
+		create_tween().tween_property(interaction_icon, "modulate", Color(1, 1, 1, 0), 0.1)
