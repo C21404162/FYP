f38021fcddb4cc0ad45d0096d167bb87e63c8c28
Joe Mulholland <C21404162@mytudublin.ie>
2025-02-28 15:31:57 +0000

    Fonts + Terrain

--- climbing-game/Scripts/interactray.gd
+++ climbing-game/Scripts/interactray.gd
@@ -1,7 +1,7 @@
 extends RayCast3D
 
 @onready var interaction_icon: TextureRect = $"../../../../Fade_interact/interaction_icon"
-@onready var dialogue_label: Label = $"../../../../Dialogue/Panel/dialogue_label"  # Reference to a Label for displaying text
+@onready var dialogue_label: Label = $"../../../../Dialogue/Panel/dialogue_label"
 
 func _ready() -> void:
 	create_tween().tween_property(interaction_icon, "modulate", Color(1, 1, 1, 0), 0.1)
@@ -11,21 +11,14 @@ func _process(delta: float) -> void:
 	if is_colliding():
 		var collider = get_collider()
 		if collider and collider.is_in_group("Interactable"):
-			# Show the interaction icon
 			create_tween().tween_property(interaction_icon, "modulate", Color(1, 1, 1, 1), 0.1)
 
-			# Check for interact input
 			if Input.is_action_just_pressed("interact"):
-				# Display text when interact is pressed
 				dialogue_label.text = "You interacted with the object!"
 				dialogue_label.visible = true
 		else:
-			# Hide the interaction icon
 			create_tween().tween_property(interaction_icon, "modulate", Color(1, 1, 1, 0), 0.1)
-			# Hide the dialogue label if not interacting with an interactable object
 			dialogue_label.visible = false
 	else:
-		# Hide the interaction icon if the raycast isn't colliding
 		create_tween().tween_property(interaction_icon, "modulate", Color(1, 1, 1, 0), 0.1)
-		# Hide the dialogue label if the raycast isn't colliding
 		dialogue_label.visible = false
