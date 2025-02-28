084a1fbbd6927055f23ec96ed81ac5629b822198
Joe Mulholland <C21404162@mytudublin.ie>
2025-02-28 14:06:49 +0000

    Barebones dialogue

--- climbing-game/Scripts/world.gd
+++ climbing-game/Scripts/world.gd
@@ -1,13 +1,13 @@
 extends Node3D
 
 @onready var color_rect = $CanvasLayer/black_screen
-@onready var fade_in = $CanvasLayer/fade_in  # Ensure this is the correct path to your AnimationPlayer
+@onready var fade_in = $Fade_interact/fade_in  
 @onready var left_hand = $Player/lefthand
 @onready var right_hand = $Player/righthand
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fade_in.play("fade_in")  # Play the "fade_in" animation
+	fade_in.play("fade_in")  
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
