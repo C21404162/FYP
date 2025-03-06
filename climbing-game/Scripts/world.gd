039db01610618976db73b35be8b54d84101f74c9
Joe Mulholland <C21404162@mytudublin.ie>
2025-03-06 15:47:24 +0000

    Sound additions

--- climbing-game/Scripts/world.gd
+++ climbing-game/Scripts/world.gd
@@ -1,14 +1,15 @@
 extends Node3D
 
-@onready var color_rect = $CanvasLayer/black_screen
+@onready var ambience = $ambience
 @onready var fade_in = $Fade_interact/fade_in  
 @onready var left_hand = $Player/lefthand
 @onready var right_hand = $Player/righthand
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	fade_in.play("fade_in")  
+	fade_in.play("fade_in")
+	ambience.play()  # Start ambient sound
+	#tween.interpolate_property(ambient_sound, "volume_db", -80, 0, 2.0)  # Fade in
+	#tween.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
